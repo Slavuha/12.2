8b9572d48d9e5bb226df312553c61d7742538657
Mazur <Slavaslidchum@gmail.com>
2022-02-20 13:16:36 +0200

    2/1

--- src/test/java/testPackage/ExampleTest.java
+++ src/test/java/testPackage/ExampleTest.java
@@ -13,18 +13,13 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 public class ExampleTest {
-    private static WebDriver driver(){
-        WebDriver driverChrome = new ChromeDriver();
-        WebDriverManager.chromedriver().setup();
-        WebDriver driverFirefox = new FirefoxDriver();
-        WebDriverManager.firefoxdriver().setup();
+    private static WebDriver driver;
 
-    return  driver();}
     @Test
     public void testExampl1Chrome() throws InterruptedException {
-        WebDriverManager.chromedriver().setup();
+      /*  WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-
+*/
         driver.get("http://automationpractice.com/");
         WebElement singInButtom = driver.findElement(By.xpath("//a [@title='Contact Us']"));
         singInButtom.click();
@@ -33,34 +28,31 @@ public class ExampleTest {
         Thread.sleep(1800);
         driver.close();
     }
-/*   @Test
+   @Test
     public void testExampl2Firefox () throws InterruptedException {
-       System.setProperty("webdriver.firefox.driver", "C:\\Users\\Мазури\\IdeaProjects\\12.1\\geckodriver.exe");
-       WebDriver driver = new FirefoxDriver();
-       driver.get("http://automationpractice.com/");
+
+      driver.get("http://automationpractice.com/");
        WebElement singInButtom = driver.findElement(By.xpath("//a [@title='Contact Us']"));
        singInButtom.click();
        assertTrue(driver.findElement(By.xpath("//textarea[@class='form-control']")).isDisplayed());
        assertTrue(driver.getCurrentUrl().contains("/index.php?controller=contact"));
        driver.close();
-   }*/
+   }
 
     @BeforeAll
   public static void setUp() {
       String browser = PropertyReader.BROWSER;
       switch (browser) {
-          case ("chrome"):
-              ChromeDriver driver;
-          {
+          case ("chrome"): {
               WebDriverManager.chromedriver().setup();
               driver = new ChromeDriver();
               break;
-       /*   }
+         }
           case ("firefox"): {
               WebDriverManager.firefoxdriver().setup();
-              driver = new ChromeDriver();
+              driver = new FirefoxDriver();
               break;
-       */   }
+          }
           default:
               throw new InvalidArgumentException("can not initialize driver available options: chrome, firefox");
       }
